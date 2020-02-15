f966041983014729c9987528cfa7bbd928f98f43
Soprativlenie <zlykabober84@gmail.com>
2020-02-15 11:57:41 +0200

    Renamed map

---
+++ src/main/java/org/wikipedia/search/console/app/JsonParser.java
@@ -0,0 +1,4 @@
+package org.wikipedia.search.console.app;/* Created by user on 15.02.20 */
+
+public class JsonParser {
+}
--- src/main/java/org/wikipedia/search/console/app/Main.java
+++ src/main/java/org/wikipedia/search/console/app/Main.java
@@ -1,10 +1,20 @@
 package org.wikipedia.search.console.app;/* Created by user on 11.02.20 */
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //TODO: Read console -- Done
+        //TODO: Generate request -- Done
+        //TODO: Send request to server
+        //TODO: Parse response
+        //TODO: Show response in console
         Scanner s = new Scanner(System.in);
-        RequestGenerator.setRequest(s.nextLine());
+        RequestGenerator requestGenerator = new RequestGenerator();
+        RequestSender requestSender = new RequestSender();
+
+        requestGenerator.setRequest(s.nextLine());
+        System.out.println(requestSender.sendRequest(requestGenerator));
     }
 }
