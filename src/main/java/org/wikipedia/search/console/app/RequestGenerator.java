2fa6892317b05ec8847ca83297342073ad255263
Soprativlenie <zlykabober84@gmail.com>
2020-02-15 12:05:09 +0200

    Minor changes

--- src/main/java/org/wikipedia/search/console/app/RequestGenerator.java
+++ src/main/java/org/wikipedia/search/console/app/RequestGenerator.java
@@ -1,14 +1,14 @@
 package org.wikipedia.search.console.app;/* Created by user on 13.02.20 */
 
 public class RequestGenerator {
-    private static String request;
+    private String generatedRequest;
 
-    public static String getRequest() {
-        return request;
+    public String getRequest() {
+        return generatedRequest;
     }
 
-    public static void setRequest(String request) {
-        RequestGenerator.request = new ApplicationConfig().urls.get("search") + request;
+    public void setRequest(String request) {
+        this.generatedRequest = new ApplicationConfig().urlSupplier.get("search") + request;
     }
 
 }
