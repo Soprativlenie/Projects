17fc0edf1c9daa2b8642bd3a435a6205aac65b9c
Soprativlenie <zlykabober84@gmail.com>
2020-02-17 22:02:05 +0200

    Implemented output Response titles to console

--- src/main/java/org/wikipedia/search/console/app/Main.java
+++ src/main/java/org/wikipedia/search/console/app/Main.java
@@ -1,10 +1,22 @@
 package org.wikipedia.search.console.app;/* Created by user on 11.02.20 */
 
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
+        RequestGenerator requestGenerator = new RequestGenerator();
+        RequestSender requestSender = new RequestSender();
+        JsonParser jsonParser = new JsonParser();
         Scanner s = new Scanner(System.in);
-        RequestGenerator.setRequest(s.nextLine());
+        requestGenerator.setRequest(s.nextLine());
+        String response = requestSender.sendRequest(requestGenerator);
+        List<String> titles = jsonParser.A(response);
+        for (String title : titles) {
+            System.out.println(title);
+        }
     }
 }
