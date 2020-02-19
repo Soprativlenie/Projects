fba56f0b7f702d808b1cf99ba87421096b9545e9
Soprativlenie <zlykabober84@gmail.com>
2020-02-19 17:00:50 +0200

    Changes according to PR discussion

--- src/main/java/org/wikipedia/search/console/app/ApplicationConfig.java
+++ src/main/java/org/wikipedia/search/console/app/ApplicationConfig.java
@@ -4,14 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ApplicationConfig {
-    private Map<String, String> urlSupplier = new HashMap<>();
-
+    private static Map<String, String> URL_SUPPLIER = new HashMap<>();
     {
-        urlSupplier.put("search", "https://ru.wikipedia.org/w/api.php?" +
+        URL_SUPPLIER.put("search", "https://ru.wikipedia.org/w/api.php?" +
                 "action=query&list=search&utf8=&format=json&srsearch=");
     }
 
-    public Map<String, String> getUrlSupplier() {
-        return urlSupplier;
+    public static Map<String, String> getUrlSupplier() {
+        return URL_SUPPLIER;
     }
 }
--- src/main/java/org/wikipedia/search/console/app/RequestGenerator.java
+++ src/main/java/org/wikipedia/search/console/app/RequestGenerator.java
@@ -3,12 +3,12 @@ package org.wikipedia.search.console.app;/* Created by user on 13.02.20 */
 public class RequestGenerator {
     private String generatedRequest;
 
-    public String getRequest() {
+    public String getGeneratedRequest() {
         return generatedRequest;
     }
 
-    public void setRequest(String request) {
-        this.generatedRequest = new ApplicationConfig().urlSupplier.get("search") + request;
+    public void setGeneratedRequest(String request) {
+        this.generatedRequest = ApplicationConfig.getUrlSupplier().get("search") + request;
     }
 
 }
