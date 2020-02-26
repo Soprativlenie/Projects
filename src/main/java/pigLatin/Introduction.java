a0b1c0489b663925b75d61c0f7f35a246d1553c9
Soprativlenie <zlykabober84@gmail.com>
2020-02-26 12:27:49 +0200

    Minor changes

--- src/main/java/pigLatin/Introduction.java
+++ src/main/java/pigLatin/Introduction.java
@@ -1,26 +1,15 @@
 package pigLatin;/* Created by user on 25.02.20 */
 
 public class Introduction {
-    private String DOT_LINE = "----------------------------------------------------";
-    private String PROGRAM_NAME = "           THIS IS PIG LATIN TRANSLATOR";
-    private String MESSAGE = "Please Input the text...";
+    private String dotLine = "----------------------------------------------------";
+    private String programName = "           THIS IS PIG LATIN TRANSLATOR";
+    private String message = "Please Input the text...";
 
-    public String getDotLine() {
-        return DOT_LINE;
-    }
-
-    public String getProgramName() {
-        return PROGRAM_NAME;
-    }
-
-    public String getMESSAGE() {
-        return MESSAGE;
-    }
 
     public void intro() {
-        System.out.println(DOT_LINE);
-        System.out.println(PROGRAM_NAME);
-        System.out.println(DOT_LINE);
-        System.out.println(MESSAGE);
+        System.out.println(dotLine);
+        System.out.println(programName);
+        System.out.println(dotLine);
+        System.out.println(message);
     }
 }
